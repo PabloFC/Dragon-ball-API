8a186e1963346e86ac3dc6f05d26b237ae119431
PabloFC <pablofc_68@hotmail.com>
2025-12-21 20:54:06 +0100

    showing characters

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Characters, Character } from './services/characters';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
   title = 'Dragon Ball API';
+  characters: Character[] = [];
+  loading = true;
+  error = '';
+
+  constructor(private charactersService: Characters) {}
+
+  ngOnInit() {
+    this.charactersService.getCharacters(1, 20).subscribe({
+      next: (response) => {
+        this.characters = response.items;
+        this.loading = false;
+        console.log(`${this.characters.length} personajes cargados`);
+      },
+      error: (err) => {
+        console.error('Error al obtener personajes:', err);
+        this.error = 'Error al cargar los personajes';
+        this.loading = false;
+      }
+    });
+  }
 }
